2a2559b953b94e9d1aae3463842887e2ad61061a
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-09-22 14:52:50 +0300

    bug fixed search bar

--- client/src/components/navBar/SearchBar.js
+++ client/src/components/navBar/SearchBar.js
@@ -18,28 +18,72 @@ const useStyles = makeStyles((theme) => ({
             marginTop: "10px",
         },
     },
-
 }));
 
 export default function SearchBar() {
     const classes = useStyles();
-    const [inputValue, SetInputValue] = useState([]);
+    const [inputValue, SetInputValue] = useState("");
     const [option, setOption] = useState([]);
     const history = useHistory();
+
+    //נכנס בקליק על אופציה
+    //נכנס בעת כיתוב אחרי לואד
     function clickHendler(e) {
+        debugger;
         if (e.keyCode === 13) {
-                history.push(`/search?params=${inputValue}`);    
+            debugger;
+            history.push(`/search?params=${inputValue}`);
         } else {
             console.log(e.currentTarget.innerText);
             history.push(`/search?params=${e.currentTarget.innerText}`);
         }
     }
 
-    async function loadOption(word) {
-        SetInputValue(word);
-        if (word) {
+    //נכנס בעת כיתוב
+    async function setter(e) {
+        debugger;
+        if (e.keyCode === 40) {
+            if (option.length !== 0) {
+                let num = option.findIndex(
+                    (element) => element.name === e.target.value
+                );
+                if (num === -1) {
+                    SetInputValue(option[0].name);
+                } else {
+                    if (num === option.length - 1) {
+                        SetInputValue(option[0].name);
+                    } else {
+                        SetInputValue(option[num + 1].name);
+                    }
+                }
+            }
+        } else if (e.keyCode === 38) {
+            if (option.length !== 0) {
+                let num = option.findIndex(
+                    (element) => element.name === e.target.value
+                );
+                if (num === -1) {
+                    SetInputValue(option[option.length - 1].name);
+                } else {
+                    if (num === 0) {
+                        SetInputValue(option[option.length - 1].name);
+                    }
+                }
+            }
+
+        } else if (e.keyCode === 13) {
+            history.push(`/search?params=${inputValue}`);
+        }
+    }
+
+    async function loadOption(e) {
+    if (e.keyCode !== 13) {
+        SetInputValue(e.target.value);
+        if (e.target.value) {
             try {
-                const { data } = await axios.get(`search?params=${word}`);
+                const { data } = await axios.get(
+                    `/search?params=${e.target.value}`
+                );
                 setOption(data);
             } catch (e) {
                 Swal.fire({
@@ -51,7 +95,7 @@ export default function SearchBar() {
         } else {
             setOption([]);
         }
-    }
+    }}
     return (
         <div style={{ width: 300 }}>
             <Autocomplete
@@ -60,13 +104,14 @@ export default function SearchBar() {
                 id="free-solo-2-demo"
                 disableClearable
                 options={option.map((obj) => obj.name)}
+                value={inputValue}
                 renderInput={(params) => (
                     <>
                         <TextField
-                            value={inputValue}
                             className={classes.textField}
                             color="secondary"
-                            onChange={(e) => loadOption(e.target.value)}
+                            onKeyUp={(e) => setter(e)}
+                            onChange={(e) => loadOption(e)}
                             {...params}
                             label={<SearchIcon />}
                             InputProps={{
