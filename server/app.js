5f62741456bd6bcb87ec4566efd42a777e735aec
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-23 10:35:00 +0200

    try deploy to heroku

--- server/app.js
+++ server/app.js
@@ -4,6 +4,15 @@ const app = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 
+const path = require("path");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "..", "client/build")));
+// Anything that doesn't match the above, send back index.html
+
 app.use("/api/", require("./api"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/client/build/index.html"));
+});
 module.exports = app;
