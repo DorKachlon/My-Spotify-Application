4831dfcdeff2be59a37f9a136c68640d85b9da72
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-23 10:54:10 +0200

    try deploy to heroku3

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
